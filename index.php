256b0c5666b4d37a5b5116d7ffb82304134f8914
ArturoDelMadrazo <33378667+Ischyrosmilus@users.noreply.github.com>
2022-11-08 13:01:18 +0100

    Rectifiction classe auth, aujourd'hui fonctionnelle

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 require_once 'vendor/autoload.php';
 
+use netvod\Auth\Auth;
 use netvod\db\ConnectionFactory;
 use netvod\render\ListeSerieRender;
 use netvod\video\lists\ListeSerie;
@@ -14,3 +15,10 @@ $series = new ListeSerie();
 $series = $series->getSeries();
 $render = new ListeSerieRender($series);
 echo $render->render();
+
+$html = '';
+
+
+
+
+echo $html;
--- src/classes/auth/Auth.php
+++ src/classes/auth/Auth.php
@@ -9,44 +9,64 @@ use netvod\user\Utilisateur;
 class Auth
 {
 
-    public static function authenticate(string $email, string $passwd2check): ?Utilisateur
+    public static function authentificate(string $email, string $passwd2check): ?Utilisateur
     {
         $db = ConnectionFactory::makeConnection();
         $query = $db->prepare("select * from Utilisateur where email = ?;");
         $query->bindParam(1, $email);
         $query->execute();
         $row = $query->fetch(PDO::FETCH_ASSOC);
-        $id = $row['IDUser'];
-        $hash = $row['motDePasse'];
-        $role = $row['role'];
-        $nom = $row['nom'];
-        $prenom = $row['prenom'];
-        $sexe = $row['sexe'];
-        if (!password_verify($passwd2check, $hash)) return null;
+        if($row === null)
+        {
+            return null;
+        }
+        else {
+            $id = $row['IDUser'];
+            $hash = $row['motDePasse'];
+            $role = $row['role'];
+            $nom = $row['nom'];
+            $prenom = $row['prenom'];
+            $sexe = $row['sexe'];
+            if (!password_verify($passwd2check, $hash)) return null;
+        }
         return new Utilisateur($id, $email, $hash, $nom, $prenom, $role, $sexe);
     }
 
-    public static function register(string $email, string $pass, string $nom, string $prenom)
+    public static function register(string $email, string $pass, string $nom, string $prenom, string $sexe)
     {
         if (strlen($pass) < 10) throw new \Exception('mot de passe trop court');
 
-        $db = ConnectionFactory::makeConnection();
-        $sql = "select * from Utilisateur where email = ?;";
-        $query = $db->prepare($sql);
-        $query->bindParam(1, $email);
-        $query->execute();
-        if ($query->fetch()) throw new \Exception("Compte deja existant");
+
 
         // encode le mot de passe
         $hash = password_hash($pass, PASSWORD_DEFAULT, ['cost' => 12]);
+        if (filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+            $db = ConnectionFactory::makeConnection();
+            $sql = "select count(email) from Utilisateur where email = ?;";
+            $query = $db->prepare($sql);
+            $query->bindParam(1, $email);
+            $query->execute();
+            $result = $query->fetch();
+            if($result[0] == 0)
+            {
+                $sql = "insert into Utilisateur values (NEXT VALUE FOR seqUser, ?, ?, 1, ?, ?, ?);";
+                $query = $db->prepare($sql);
+                $query->bindParam(1, $email);
+                $query->bindParam(2, $hash);
+                $query->bindParam(3, $nom);
+                $query->bindParam(4, $prenom);
+                $query->bindParam(5, $sexe);
+                $query->execute();
+                $query->closeCursor();
+                echo "Inscription réussie";
+            }
+            else
+            {
+                echo "Email déjà utilisé";
+            }
+        }
 
-        // inserer l'utilisateur
-        $sql = "insert into Utilisateur set email = ?, motDePasse=?, role=1, nom=?, prenom=?;";
-        $query = $db->prepare($sql);
-        $query->bindParam(1, $email);
-        $query->bindParam(2, $hash);
-        $query->bindParam(3, $nom);
-        $query->bindParam(4, $prenom);
-        $query->execute();
+
     }
 }
