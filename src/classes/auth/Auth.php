d383dc627cf83d197a2a62b25dc6d783424f734e
Nzosim <nzosim54@gmail.com>
2022-11-08 11:22:57 +0100

    modif auth

--- src/classes/auth/Auth.php
+++ src/classes/auth/Auth.php
@@ -12,17 +12,21 @@ class Auth
     public static function authenticate(string $email, string $passwd2check): ?Utilisateur
     {
         $db = ConnectionFactory::makeConnection();
-        $query = $db->prepare("select password, role from Utilisateur where email = ?;");
+        $query = $db->prepare("select * from Utilisateur where email = ?;");
         $query->bindParam(1, $email);
         $query->execute();
         $row = $query->fetch(PDO::FETCH_ASSOC);
-        $hash = $row['password'];
+        $id = $row['IDUser'];
+        $hash = $row['motDePasse'];
         $role = $row['role'];
+        $nom = $row['nom'];
+        $prenom = $row['prenom'];
+        $sexe = $row['sexe'];
         if (!password_verify($passwd2check, $hash)) return null;
-        return new Utilisateur($role, $email, $hash);
+        return new Utilisateur($id, $email, $hash, $nom, $prenom, $role, $sexe);
     }
 
-    public static function register(string $email, string $pass)
+    public static function register(string $email, string $pass, string $nom, string $prenom)
     {
         if (strlen($pass) < 10) throw new \Exception('mot de passe trop court');
 
@@ -37,10 +41,12 @@ class Auth
         $hash = password_hash($pass, PASSWORD_DEFAULT, ['cost' => 12]);
 
         // inserer l'utilisateur
-        $sql = "insert into Utilisateur set email = ?, passwd=?;";
+        $sql = "insert into Utilisateur set email = ?, motDePasse=?, role=1, nom=?, prenom=?;";
         $query = $db->prepare($sql);
         $query->bindParam(1, $email);
         $query->bindParam(2, $hash);
+        $query->bindParam(3, $nom);
+        $query->bindParam(4, $prenom);
         $query->execute();
     }
 }
