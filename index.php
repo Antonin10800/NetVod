1edf3a8be18ddf309656c6727dca5eca01581fb3
ArturoDelMadrazo <33378667+Ischyrosmilus@users.noreply.github.com>
2022-11-08 13:12:34 +0100

    Suppressions fichiers inutile

--- index.php
+++ index.php
@@ -3,22 +3,7 @@ declare(strict_types=1);
 
 require_once 'vendor/autoload.php';
 
-use netvod\Auth\Auth;
 use netvod\db\ConnectionFactory;
-use netvod\render\ListeSerieRender;
-use netvod\video\lists\ListeSerie;
 
 ConnectionFactory::setConfig();
 
-
-$series = new ListeSerie();
-$series = $series->getSeries();
-$render = new ListeSerieRender($series);
-echo $render->render();
-
-$html = '';
-
-
-
-
-echo $html;
