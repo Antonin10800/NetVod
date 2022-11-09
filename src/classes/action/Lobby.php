454c437330e14cedfb2e8e3c33fdf5e3f26275dd
Nzosim <nzosim54@gmail.com>
2022-11-09 21:26:02 +0100

    changement Lobby.php

--- src/classes/action/Lobby.php
+++ src/classes/action/Lobby.php
@@ -12,30 +12,31 @@ class Lobby implements Action
 {
     public function execute(): string
     {
-        $html = '<!DOCTYPE html>';
-        $html .= '<html lang="fr"> <head>';
-        $html .= '<meta charset="UTF-8">';
-        $html .= '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
-        $html .= '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-        $html .= '<title>NetVod</title>';
-        $html .= '<script src="src/js/profile.js"></script>';
-        $html .= '<link rel="stylesheet" href="src/css/loby.css">';
-        $html .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />';
-        $html .= '</head><body>';
+        $html = <<<END
+            <!DOCTYPE html>
+            <html lang="fr"> <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>NetVod</title>
+            <script src="src/js/profile.js"></script>
+            <link rel="stylesheet" href="src/css/loby.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            </head><body>
 
-        $html .= '<header>';
-        $html .= '<div class="headerLeft">';
-        $html .= '<h1>NetVod</h1>';
-        $html .= '</div>';
-        $html .= '<div class="headerRight">';
-        //$html .= '<a href="?action=profile"><i  class="fa-solid fa-user"></i></a>';
-        $html .= '<a onclick="profilePage()"><i  class="fa-solid fa-user"></i></a>';
+            <header>
+            <div class="headerLeft">
+            <h1>NetVod</h1>
+            </div>
+            <div class="headerRight">
+            <a onclick="profilePage()"><i  class="fa-solid fa-user"></i></a>
+            </div>
+            </header>
+            END;
 
-
-        $html .= '</div>';
-        $html .= '</header>';
         $listeSerie = new ListeSerie();
         $listeSerieRender = new ListeSerieRender($listeSerie->getSeries());
+
         //$html .= $listeSerieRender->render();
         $html .= "<div class=\"content\">";
         $html .= $listeSerieRender->renderGenre("Horreur");
@@ -46,10 +47,11 @@ class Lobby implements Action
 
         $html .= '<div class="profile">';
         $html .= '<a onclick="hideProfilePage()"><i class="fa-solid fa-xmark"></i></a>';
-        $html .= '<a>Email : ' . unserialize($_SESSION['user'])->__get('email') . '</a><br>';
-        $html .= '<a>Nom : ' . unserialize($_SESSION['user'])->__get('nom') . '</a><br>';
-        $html .= '<a>Prenom : ' . unserialize($_SESSION['user'])->__get('prenom') . '</a><br>';
-        $html .= '<a>Sexe : ' . unserialize($_SESSION['user'])->__get('sexe') . '</a><br>';
+        $html .= '<a>Email : ' . unserialize($_SESSION['user'])->email . '</a><br>';
+        $html .= '<a>Nom : ' . unserialize($_SESSION['user'])->nom . '</a><br>';
+        $html .= '<a>Prenom : ' . unserialize($_SESSION['user'])->prenom . '</a><br>';
+        $html .= '<a>Sexe : ' . unserialize($_SESSION['user'])->sexe . '</a><br>';
+
         // bouton de déconnexion
         $html .= '<a class="btn-deconnexion" href="?action=deconnexion"><i class="fa-solid fa-arrow-right-from-bracket"></i>  Déconnexion</a>';
         $html .= '</div>';
