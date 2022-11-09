5bb197cdd742ca3a513094cf018d5881509201c8
Bozeyy <spereaux2@gmail.com>
2022-11-09 11:34:19 +0100

    ajout du css pour les erreur quand la personne na pas remplit tout les champs

--- src/classes/action/Inscription.php
+++ src/classes/action/Inscription.php
@@ -48,74 +48,33 @@ class Inscription implements Action
                 $html .= '</form>';
 
 
-            }elseif (empty($_POST['password'])){
+            }elseif (empty($_POST['password']) || empty($_POST['password2']) || empty($_POST['nom']) || empty($_POST['prenom'])) {
                 $html .= '<form class="form" method="post" action="?action=inscription">';
                 $html .=    '<div class="title"><h1>Inscription</h1></div>';
                 $html .=    '<p>Email</p><input class="input" type="email" name="email" >';
                 $html .=    '<p>Password</p><input type="password" name="password" >';
-                $html .=    '<div class="ErreurInscription>"<p>Le champ doit être renseigné</p></div>';
                 $html .=    '<p>Password confirmation</p><input class="input" type="password" name="password2" >';
+                $html .=    '<div class="name"><div class="part"><p>Nom</p><input type="text" name="nom"></div>';
+                $html .=    '<div class="part"><p>Prenom</p><input class="input" type="text" name="prenom"></div></div>';
+                $html .=    '<div class="gender"><input class="input" type="radio" name="genre" value="Femme">Femme';
+                $html .=    '<input type="radio" class="input" name="genre" value="Homme">Homme<br></div>';
+                $html .= '<div class="error"><p>Tous les champs doivent être renseignés !</p></div>';
+                $html .=    '<button id="disable" type="submit">Inscription</button>';
+                $html .= '</form>';
+            } else if($_POST['password'] != $_POST['password2']) {
+                $html .= '<form class="form" method="post" action="?action=inscription">';
+                $html .=    '<div class="title"><h1>Inscription</h1></div>';
+                $html .=    '<p>Email</p><input class="input" type="email" name="email" >';
+                $html .=    '<p>Password</p><input type="password" name="password" >';
+                $html .=    '<p>Password confirmation</p><input class="input" type="password" name="password2" >';
+                $html .=    '<div class="error"><p>Les mots de passes ne sont pas identiques !</p></div>';
                 $html .=    '<div class="name"><div class="part"><p>Nom</p><input type="text" name="nom"></div>';
                 $html .=    '<div class="part"><p>Prenom</p><input class="input" type="text" name="prenom"></div></div>';
                 $html .=    '<div class="gender"><input class="input" type="radio" name="genre" value="Femme">Femme';
                 $html .=    '<input type="radio" class="input" name="genre" value="Homme">Homme<br></div>';
                 $html .=    '<button type="submit">Inscription</button>';
                 $html .= '</form>';
-            }elseif (empty($_POST['password2'])){
-                $html .= '<form class="form" method="post" action="?action=inscription">';
-                $html .=    '<div class="title"><h1>Inscription</h1></div>';
-                $html .=    '<p>Email</p><input class="input" type="email" name="email" >';
-                $html .=    '<p>Password</p><input type="password" name="password" >';
-                $html .=    '<p>Password confirmation</p><input class="input" type="password" name="password2" >';
-                $html .=    '<div class="ErreurInscription>"<p>Le champ doit être renseigné</p></div>';
-                $html .=    '<div class="name"><div class="part"><p>Nom</p><input type="text" name="nom"></div>';
-                $html .=    '<div class="part"><p>Prenom</p><input class="input" type="text" name="prenom"></div></div>';
-                $html .=    '<div class="gender"><input class="input" type="radio" name="genre" value="Femme">Femme';
-                $html .=    '<input type="radio" class="input" name="genre" value="Homme">Homme<br></div>';
-                $html .=    '<button type="submit">Inscription</button>';
-                $html .= '</form>';
-            }elseif (empty($_POST['nom'])){
-                $html .= '<form class="form" method="post" action="?action=inscription">';
-                $html .=    '<div class="title"><h1>Inscription</h1></div>';
-                $html .=    '<p>Email</p><input class="input" type="email" name="email" >';
-                $html .=    '<p>Password</p><input type="password" name="password" >';
-                $html .=    '<p>Password confirmation</p><input class="input" type="password" name="password2" >';
-                $html .=    '<div class="name"><div class="part"><p>Nom</p><input type="text" name="nom"></div>';
-                $html .=    '<div class="ErreurInscription>"<p>Le champ doit être renseigné</p></div>';
-                $html .=    '<div class="part"><p>Prenom</p><input class="input" type="text" name="prenom"></div></div>';
-                $html .=    '<div class="gender"><input class="input" type="radio" name="genre" value="Femme">Femme';
-                $html .=    '<input type="radio" class="input" name="genre" value="Homme">Homme<br></div>';
-                $html .=    '<button type="submit">Inscription</button>';
-
-            }elseif (empty($_POST['prenom'])){
-                $html .= '<form class="form" method="post" action="?action=inscription">';
-                $html .=    '<div class="title"><h1>Inscription</h1></div>';
-                $html .=    '<p>Email</p><input class="input" type="email" name="email" >';
-                $html .=    '<p>Password</p><input type="password" name="password" >';
-                $html .=    '<p>Password confirmation</p><input class="input" type="password" name="password2" >';
-                $html .=    '<div class="name"><div class="part"><p>Nom</p><input type="text" name="nom"></div>';
-                $html .=    '<div class="part"><p>Prenom</p><input class="input" type="text" name="prenom"></div></div>';
-                $html .=    '<div class="ErreurInscription>"<p>Le champ doit être renseigné</p></div>';
-                $html .=    '<div class="gender"><input class="input" type="radio" name="genre" value="Femme">Femme';
-                $html .=    '<input type="radio" class="input" name="genre" value="Homme">Homme<br></div>';
-                $html .=    '<button type="submit">Inscription</button>';
-            }
-
-            else if($_POST['password'] != $_POST['password2']) {
-                $html .= '<form class="form" method="post" action="?action=inscription">';
-                $html .=    '<div class="title"><h1>Inscription</h1></div>';
-                $html .=    '<p>Email</p><input class="input" type="email" name="email" >';
-                $html .=    '<p>Password</p><input type="password" name="password" >';
-                $html .=    '<p>Password confirmation</p><input class="input" type="password" name="password2" >';
-                $html .=   '<div class="ErreurInscription>"<p>Les mots de passe ne sont pas identiques</p></div>';
-                $html .=    '<div class="name"><div class="part"><p>Nom</p><input type="text" name="nom"></div>';
-                $html .=    '<div class="part"><p>Prenom</p><input class="input" type="text" name="prenom"></div></div>';
-                $html .=    '<div class="gender"><input class="input" type="radio" name="genre" value="Femme">Femme';
-                $html .=    '<input type="radio" class="input" name="genre" value="Homme">Homme<br></div>';
-                $html .=    '<button type="submit">Inscription</button>';
-                $html .= '</form>';
-            }
-            else {
+            } else {
                 $email = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
                 $pass = filter_var($_POST['password'],FILTER_SANITIZE_STRING);
                 $nom = filter_var($_POST['nom'],FILTER_SANITIZE_STRING);
