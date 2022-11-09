138d5250f89aca77d462740c63f424cfa44a9201
ElBouroumi Anas <gon.fxx@gmail.com>
2022-11-09 14:40:12 +0100

    commentaire afficher série

--- src/classes/action/AfficherSerie.php
+++ src/classes/action/AfficherSerie.php
@@ -3,9 +3,10 @@
 namespace netvod\action;
 
 use netvod\action\Action;
+use netvod\video\episode\Episode;
 use netvod\db\ConnectionFactory;
 use netvod\render\RenderInfoSerie;
-use netvod\video\episode\Episode;
+
 use netvod\video\episode\Serie;
 use netvod\video\lists\ListeSerie;
 
@@ -18,8 +19,10 @@ class AfficherSerie implements Action
     {
         $html = '';
 
+        //on charge les épisode de la série:
         $this->chargerEpisode();
 
+        //on crée un série render Info:
         $serieRender = new RenderInfoSerie($this->serieCourante);
         $html = $serieRender->render();
 
@@ -29,21 +32,30 @@ class AfficherSerie implements Action
 
     public function chargerEpisode()
     {
+        //on récupere l'id série
         $html = '';
         $idSerie = filter_var($_GET['idSerie'],FILTER_SANITIZE_NUMBER_INT);
 
+        //on récupere la liste des séries:
         $listeSerie = ListeSerie::getInstance();
         $series = $listeSerie->getSeries();
 
+        //on parcours toute les séries
         foreach ($series as $s) {
+            //si l'id de la série est égal a la l'id série du GET:
             if ($s->__get('IDserie') == $idSerie)
             {
+                //on mets la série trouve dans une variable
                 $serieTrouve = $s;
+                //on trouve la série qu'on cherche
                 $this->serieCourante = $serieTrouve;
+                //on s'arrete
                 break;
             }
         }
-        if ($serieTrouve->__get('listeEpisode') == null) {
+        //si la série a une liste null:
+        if ($this->serieCourante->__get('listeEpisode') == null) {
+            //on récupére
             $query = "select E.idEpisode, titre, duree, image,numEp from Serie2Episode inner join 
                             Episode E on Serie2Episode.IDEpisode = E.idEpisode
                             where Serie2Episode.IDSerie = ?";
@@ -56,7 +68,7 @@ class AfficherSerie implements Action
                 $episode = new Episode($row['idEpisode'], $row['duree'], $row['titre'], $row['image'], $row['numEp']);
                 $listeEpisode[] = $episode;
             }
-            $serieTrouve->__set('listeEpisode', $listeEpisode);
+            $this->serieCourante->__set('listeEpisode', $listeEpisode);
 
         }
     }
