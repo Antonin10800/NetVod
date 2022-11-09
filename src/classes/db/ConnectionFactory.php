e645570984243923e497efacb8cef5d17508b254
Nzosim <nzosim54@gmail.com>
2022-11-09 21:38:22 +0100

    javadoc ConnectionFactory.php

--- src/classes/db/ConnectionFactory.php
+++ src/classes/db/ConnectionFactory.php
@@ -4,17 +4,29 @@ namespace netvod\db;
 
 use PDO;
 
+/**
+ * class ConnectionFactory
+ * permet la connection a la base de données
+ */
 class ConnectionFactory
 {
-
+    // variable qui contiennent la connection a la base de données
     private static $db;
     private static $config = [];
 
+    /**
+     * methode setConfig qui permet de configurer le fichier utilisé qui contient les informations de connection
+     * @return void
+     */
     public static function setConfig(): void
     {
         self::$config = parse_ini_file("db.config.ini");
     }
 
+    /**
+     * methode makeConnection qui permet de se connecter a la base de données
+     * @return PDO la connection a la base de données
+     */
     public static function makeConnection() : PDO
     {
         if (self::$db == null)
