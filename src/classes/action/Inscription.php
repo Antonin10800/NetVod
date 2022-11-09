315297a16bc3b646d59ee1dea2ee15672a17a7ce
Nzosim <nzosim54@gmail.com>
2022-11-09 10:18:16 +0100

    css

--- src/classes/action/Inscription.php
+++ src/classes/action/Inscription.php
@@ -18,16 +18,16 @@ class Inscription implements Action
         $html .= '</head><body background="src/classes/images/css/netfix_background.jpeg">';
         
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-            $html .= '<form method="post" action="?action=inscription">';
-            $html .= '<div class="title"><h1>Inscription</h1></div>';
-            $html .= '<p>email</p><input type="email" name="email"  placeholder="Email">';
-            $html .= '<p>password</p><input type="password" name="password"  placeholder="Mot de passe">';
-            $html .= '<p>password confirmation</p><input type="password" name="password2"  placeholder="Mot de passe">';
-            $html .= '<div class="name"><div class="part"><p>nom</p><input type="text" name="nom"  placeholder="Nom"></div>';
-            $html .= '<div class="part"><p>prenom</p><input type="text" name="prenom"  placeholder="Prenom"></div></div>';
-            $html .= '<div class="gender"><input type="radio" name="genre" value="Femme">Femme';
-            $html .= '<input type="radio" name="genre" value="Homme">Homme<br></div>';
-            $html .= '<button type="submit">Inscription</button>';
+            $html .= '<form class="form" method="post" action="?action=inscription">';
+            $html .=    '<div class="title"><h1>Inscription</h1></div>';
+            $html .=    '<p>email</p><input class="input" type="email" name="email" >';
+            $html .=    '<p>password</p><input type="password" name="password" >';
+            $html .=    '<p>password confirmation</p><input class="input" type="password" name="password2" >';
+            $html .=    '<div class="name"><div class="part"><p>nom</p><input type="text" name="nom"></div>';
+            $html .=    '<div class="part"><p>prenom</p><input class="input" type="text" name="prenom"></div></div>';
+            $html .=    '<div class="gender"><input class="input" type="radio" name="genre" value="Femme">Femme';
+            $html .=    '<input type="radio" class="input" name="genre" value="Homme">Homme<br></div>';
+            $html .=    '<button type="submit">Inscription</button>';
             $html .= '</form>';
         } else if (($_SERVER['REQUEST_METHOD'] == 'POST')) {
             if($_POST['password'] != $_POST['password2']) {
