816d70f9ec9fd1ae9cd5f8f848cc645922f3cfc7
Bozeyy <spereaux2@gmail.com>
2022-11-09 13:41:49 +0100

    ajout d'un meilleur affichage pour les genre de séries dans le lobby

--- src/classes/action/Lobby.php
+++ src/classes/action/Lobby.php
@@ -19,8 +19,18 @@ class Lobby implements Action
         $html .= '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
         $html .= '<title>Inscription</title>';
         $html .= '<link rel="stylesheet" href="src/css/loby.css">';
+        $html .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />';
         $html .= '</head><body>';
 
+        $html .= '<header>';
+        $html .= '<div class="headerLeft">';
+        $html .= '<h1>NetVod</h1>';
+        $html .= '</div>';
+        $html .= '<div class="headerRight">';
+        $html .= '<a><i class="fa-solid fa-user"></i></a>';
+
+        $html .= '</div>';
+        $html .= '</header>';
         $listeSerie = new ListeSerie();
         $listeSerie->remplirListe();
         $listeSerieRender = new ListeSerieRender($listeSerie->getSeries());
@@ -28,6 +38,8 @@ class Lobby implements Action
         $html .= "<div class=\"content\">";
         $html .= $listeSerieRender->renderGenre("Horreur");
         $html .= $listeSerieRender->renderGenre("Comédie");
+        $html .= $listeSerieRender->renderGenre("Thriller");
+        $html .= $listeSerieRender->renderGenre("Divertissement");
         $html .= "</div>";
 
         $html .= '</body></html>';
