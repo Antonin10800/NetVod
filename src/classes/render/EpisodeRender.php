cc5c3ae690d39e417a98c1eeda4c590d9e9bd545
Antonin10800 <anto10800@gmail.com>
2022-11-10 13:41:18 +0100

    Rien

--- src/classes/render/EpisodeRender.php
+++ src/classes/render/EpisodeRender.php
@@ -4,19 +4,27 @@ namespace netvod\render;
 
 use netvod\video\episode\Episode;
 
-class EpisodeRender implements Render {
+class EpisodeRender implements Render
+{
 
     private Episode $episode;
 
-    public function __construct(Episode $episode){
+    public function __construct(Episode $episode)
+    {
         $this->episode = $episode;
     }
 
-    public function render() : string {
-        return "<div class=\"idEpisode\"> {$this->episode->titre}<br>"
-             . "{$this->episode->duree}<br>"
-             . "{$this->episode->image}<br>"
-             . "{$this->episode->numeroEp}<br><br> </div>";
+    public function render(): string
+    {
+        return <<<HTML
+                <div class=\"idEpisode\"> {$this->episode->titre}<br>
+                {$this->episode->duree}<br>
+                {$this->episode->image}<br>
+                {$this->episode->numeroEp}<br><br> </div>
+                <video width=\"320\" height=\"240\" controls>
+                <source src="src/classes/video/episode/Video.mp4" type="video/mp4">
+HTML;
+
     }
 
 }
