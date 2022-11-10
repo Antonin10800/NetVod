b8add0bbd6606efaf1cbfb419206abf39bc500cd
ArturoDelMadrazo <33378667+Ischyrosmilus@users.noreply.github.com>
2022-11-10 09:57:56 +0100

    Ajout actualiser avis

--- src/classes/action/Favoris.php
+++ src/classes/action/Favoris.php
@@ -4,7 +4,9 @@ namespace netvod\action;
 
 use netvod\Auth\Auth;
 use netvod\db\ConnectionFactory;
+use netvod\render\ListeSerieRender;
 use netvod\video\episode\Serie;
+use netvod\video\lists\ListeSerie;
 
 class Favoris implements Action
 {
@@ -17,25 +19,37 @@ class Favoris implements Action
         //on récupere l'id User et l'id Serie
         $idUser = $utilisateur->IDuser;
         $idSerie = filter_var($_GET['idSerie'],FILTER_SANITIZE_NUMBER_INT);
+
+        $db = ConnectionFactory::makeConnection();
+
         //on vérifie si la série est deja en favoris
         if(self::pasDeFavoris())
         {
             $query = "INSERT INTO Favoris VALUES(?,?)";
             //on execute la query
-            $db = ConnectionFactory::makeConnection();
             $statement = $db->prepare($query);
             $statement->bindParam(1,$idUser);
             $statement->bindParam(2,$idSerie);
             $statement->execute();
-            $serie = Serie::getSerie($idSerie);
-            $utilisateur->ajouterFavoris($serie);
+
+            $instance = ListeSerie::getInstance();
+            $serie = $instance->getSeries();
+
+
+            foreach ($serie as $item) {
+                if($item->IDserie == $idSerie)
+                {
+                    $serieCourante = $item;
+                }
+            }
+
+            $utilisateur->ajouterFavoris($serieCourante);
             $_SESSION['user'] = serialize($utilisateur);
         }
         else
         {
             $query = "DELETE FROM Favoris WHERE IDUser = ? AND IDSerie= ?";
             //on execute la query
-            $db = ConnectionFactory::makeConnection();
             $statement = $db->prepare($query);
             $statement->bindParam(1,$idUser);
             $statement->bindParam(2,$idSerie);
