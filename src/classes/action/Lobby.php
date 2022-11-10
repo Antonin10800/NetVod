4f95d0a851a89f489da8ded1fbcdd5398e2b8e6b
Bozeyy <spereaux2@gmail.com>
2022-11-10 16:53:23 +0100

    style de la bar de recherche

--- src/classes/action/Lobby.php
+++ src/classes/action/Lobby.php
@@ -34,11 +34,11 @@ class Lobby implements Action
                 <div class="headerLeft">
                     <a>NETVOD</a>
                 </div>
-               <div class="headerMid">
-               <form method="post" action="?action=connexion">
-                    <input type="search" name="inputRecherche" class="inputRecherche" required>
-                    <button type="submit" name="submit" value="recherche"><i class="fa-solid fa-search"></i></button>
-                </form>
+                <div class="headerMid">
+                    <form method="post" action="?action=connexion">
+                        <input type="search" name="inputRecherche" class="inputRecherche" required>
+                        <button class="btnRecherche" type="submit" name="submit" value="recherche"><i class="fa-solid fa-search"></i></button>
+                    </form>
                 </div>
                 <div class="headerRight">
                     <a onclick="profilePage()"><i  class="fa-solid fa-user"></i></a>
