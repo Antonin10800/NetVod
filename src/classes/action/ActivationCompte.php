3d55f84169d56c07a45af1202c8da614b8c8bc77
Nzosim <nzosim54@gmail.com>
2022-11-10 15:42:09 +0100

    doc ActivationCompte.php

--- src/classes/action/ActivationCompte.php
+++ src/classes/action/ActivationCompte.php
@@ -4,11 +4,18 @@ namespace netvod\action;
 
 use netvod\Auth\Auth;
 
-class ActivationCompte implements Action
-{
-    public function execute(): string
-    {
+/**
+ * class ActivationCompte
+ * permet d'activer le compte d'un utilisateur
+ */
+class ActivationCompte implements Action {
 
+    /**
+     * methode execute permet d'avoir un rendu html
+     * @return string le rendu html
+     */
+    public function execute(): string {
+        // header de la page html
         $html = <<<END
             <!DOCTYPE html>
             <html lang="fr"> <head>
@@ -21,8 +28,10 @@ class ActivationCompte implements Action
             </head><body background="src/images/css/netfix_background.jpeg">
             END;
 
+        // nettoyage du token
         $token = filter_var($_GET['token'],FILTER_SANITIZE_STRING);
 
+        // si la requete est de type GET on affiche le bouton pour activer le token
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             $html .= <<<END
                 <form class="form" method="post" action="?action=activation&token=$token">
@@ -30,7 +39,10 @@ class ActivationCompte implements Action
                 </form></body></html>
                 END;
         } else if (($_SERVER['REQUEST_METHOD'] == 'POST')) {
+            // si c'est de type post on active le compte avec le token
+            // et la fonction activerCompte qui verifie si le token est encore  valide avant d'activer le compte
             Auth::activerCompte($token);
+            // on redirige vers la page de connexion
             header("Location: ?action=connexion");
         }
 
